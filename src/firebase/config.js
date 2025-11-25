5b6e1229cedbbd1ec88e820f3b98c2822d929125
nosib55 <lmnosib555@gmail.com>
2025-11-25 17:36:54 +0600

    configared

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,15 @@
-// Firebase Core SDK
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbHDzsUidP_u5jVNELiqb6ASv0ZmdZVE4",
-  authDomain: "swapx-d1672.firebaseapp.com",
-  projectId: "swapx-d1672",
-  storageBucket: "swapx-d1672.appspot.com",
-  messagingSenderId: "763881251026",
-  appId: "1:763881251026:web:573387bd6f439df2c60d0b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Authentication instance
 export const auth = getAuth(app);
